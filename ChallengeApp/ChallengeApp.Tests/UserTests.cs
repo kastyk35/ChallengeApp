10004012eb0c970b43c39a067f5a91121c6ee8e2
Janusz Kastyk <jamalynuszko@gmail.com>
2023-11-27 02:09:13 +0100

    Dzień 9 - praca domowa

--- ChallengeApp/ChallengeApp.Tests/UserTests.cs
+++ ChallengeApp/ChallengeApp.Tests/UserTests.cs
@@ -1,22 +1,56 @@
 namespace ChallengeApp.Tests
 {
-    public class UserTests
+    public class EmployeeTests
     {
         [Test]
-        public void WhenUserEarnsScores_ThenShouldReturnCorrectResult()
+        public void UserEarnsPositiveAndNegativeScores_ShouldReturnCorrectResult()
         {
             // arrange - przygotowanie
-            var user = new User("Kastyk");
-            user.AddScore(15);
-            user.AddScore(-6);
-            user.AddScore(9);
-            user.AddScore(-12);
+            var emp = new Employee("Janusz");
+            emp.AddGrade(15);
+            emp.AddGrade(-6);
+            emp.AddGrade(9);
+            emp.AddGrade(-12);
 
             // act - wykonanie
-            var result = user.Result;
+            var result = emp.Result;
 
             //assert - przewidywany wynik
             Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void UserNameCompareTest()
+        {
+            // arrange - przygotowanie
+            var user1 = GetUser("Janek");
+            var user2 = GetUser("Jaœko");
+
+            // act - wykonanie
+           
+            //assert - przewidywany wynik
+            Assert.AreNotEqual(user1, user2);
+        }
+
+        private Employee GetUser(string name)
+        {
+            return new Employee(name);
+        }
+
+        [Test]
+        public void AverageGradeTest()
+        {
+            var emp = new Employee("John");
+            emp.AddGrade(15);
+            emp.AddGrade(-6);
+            emp.AddGrade(9);
+            emp.AddGrade(-12);
+
+            // act - wykonanie
+            var result = emp.GetStatistics();
+
+            //assert - przewidywany wynik
+            Assert.AreEqual(1.5, result.Average);
         }
     }
 }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,44 @@
 ﻿using ChallengeApp;
 
-User user1 = new User("Adam", "123456a");
-User user2 = new User("Monika", "123456a");
-User user3 = new User("Zuzia", "123456a");
+var employee1 = new Employee("Janusz", "Kastyk");
+var employee2 = new Employee("Jaśko", "Dębowy");
+var employee3 = new Employee("Mnietek", "Kargulowski");
 
-user1.AddScore(5);
-user1.AddScore(2);
-user1.AddScore(1);
+employee1.AddGrade(2);
+employee1.AddGrade(5);
+employee1.AddGrade(6);
+employee1.AddGrade(-4);
+employee1.AddGrade(9);
 
-user2.AddScore(2);
-user2.AddScore(5);
-user2.AddScore(9);
+employee2.AddGrade(10);
+employee2.AddGrade(-7);
+employee2.AddGrade(4);
+employee2.AddGrade(-2);
+employee2.AddGrade(3);
 
-user3.AddScore(11);
-user3.AddScore(1);
-user3.AddScore(1);
+employee3.AddGrade(4);
+employee3.AddGrade(-11);
+employee3.AddGrade(12);
+employee3.AddGrade(7);
+employee3.AddGrade(-3);
 
+var statistics1 = employee1.GetStatistics();
+var statistics2 = employee2.GetStatistics();
+var statistics3 = employee3.GetStatistics();
+
+Console.WriteLine("Dane dla {0} {1}: ", employee1.Name, employee1.Surname);
+Console.WriteLine($"Ocena minimalna to {statistics1.Min}");
+Console.WriteLine($"Ocena maksymalna to {statistics1.Max}");
+Console.WriteLine($"Ocena średnia: {statistics1.Average:N2}");
+
+Console.WriteLine("\nDane dla {0} {1}: ", employee2.Name, employee2.Surname);
+Console.WriteLine($"Ocena minimalna to {statistics2.Min}");
+Console.WriteLine($"Ocena maksymalna to {statistics2.Max}");
+Console.WriteLine($"Ocena średnia: {statistics2.Average:N2}");
+
+Console.WriteLine("\nDane dla {0} {1}: ", employee3.Name, employee3.Surname);
+Console.WriteLine($"Ocena minimalna to {statistics3.Min}");
+Console.WriteLine($"Ocena maksymalna to {statistics3.Max}");
+Console.WriteLine($"Ocena średnia: {statistics3.Average:N2}");
+
+Console.ReadKey();
